ae77a0b06e499af6cefa323422ca90e63458bd6c
Ngozi Lilian Okafor <okaforngozi1704@gmail.com>
2026-07-17 00:58:44 +0700

    Heart beat tracking UI

--- stepsafe/src/app/index.tsx
+++ stepsafe/src/app/index.tsx
@@ -2,5 +2,7 @@ import ProfileScreen from '../screens/ProfileScreen';
 import TrackingScreen from '../screens/TrackScreen';
 
 export default function HomeScreen() {
+ // return <TrackingScreen />;
   return <ProfileScreen />;
+
 }
